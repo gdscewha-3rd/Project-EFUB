adad0626f1fbcff07a56f6d5db19a0d0bb29314c
roum02 <roum02@ewhain.net>
2021-11-07 14:46:25 +0900

    recruitBox 생성

--- efub-homepage/src/pages/RecruitmentPage.js
+++ efub-homepage/src/pages/RecruitmentPage.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+
+const RecruitBox = styled.div`
+  width: 90%;
+  height: 30rem;
+  background-color: rgba(225, 225, 225, 0.15);
+`;
 
 const RecruitmentPage = () => {
-    return (
-        <>
-            <h1>RecruitmentPage</h1>
-        </>
-    );
+  return (
+    <>
+      <h1>RecruitmentPage</h1>
+      <RecruitBox />
+    </>
+  );
 };
 
 export default RecruitmentPage;
